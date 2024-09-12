7c188cf7b72ef30dadaa6b499c2e334ffb939395
wecmyrc <wecmyrc@gmail.com>
2024-09-12 11:09:34 +0300

    Initial commit

---
+++ bin/setup.py
@@ -0,0 +1,210 @@
+import os
+import platform
+import shutil
+import subprocess
+import sys
+from distutils.command.build import build
+
+from setuptools import setup
+
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+except ImportError:
+    cmd_class = {}
+else:
+
+    class BdistWheel(_bdist_wheel):
+
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            # Mark us as not a pure python package
+            self.root_is_pure = False
+
+        def get_tag(self):
+            python, abi, plat = _bdist_wheel.get_tag(self)
+            python = "py{}".format(sys.version_info[0])
+            return python, "none", plat
+
+    cmd_class = {"bdist_wheel": BdistWheel}
+
+
+PACKAGE_PATH = "rhvoice_wrapper_bin"
+RHVOICE = "RHVoice"
+RHVOICE_TAG = "1.14.0"
+RHVOICE_URL = "https://github.com/RHVoice/RHVoice.git"
+LIB = "lib"
+EXT = "dylib" if platform.system().lower() == "darwin" else "so"
+
+
+def is_64bit():
+    return sys.maxsize > 2**32
+
+
+def is_win():
+    return os.name == "nt"
+
+
+def library_selector(rhvoice_path):
+    starts = os.path.join(rhvoice_path, "build", platform.system().lower())
+    if is_win():
+        targets = [
+            os.path.join(
+                starts, "x86_64" if is_64bit() else "x86", "lib", "RHVoice.dll"
+            )
+        ]
+    else:
+        targets = [
+            os.path.join(starts, "core", "libRHVoice_core.{}".format(EXT)),
+            os.path.join(starts, "lib", "libRHVoice.{}".format(EXT)),
+        ]
+    return targets
+
+
+def scons_selector():
+    cmd = ["scons", "audio_libs=none"]
+    if is_win():
+        cmd.append("enable_xp_compat=no")
+        cmd.append("enable_x64={}".format("yes" if is_64bit() else "no"))
+    return cmd
+
+
+def check_build(libraries_path):
+    for target in libraries_path:
+        if not os.path.isfile(target):
+            return "File {} not found".format(target)
+
+
+def executor(cmd, cwd):
+    err = None
+    try:
+        run = subprocess.call(cmd, cwd=cwd, shell=is_win())
+    except Exception as e:
+        err = e
+    else:
+        if run != 0:
+            err = "code: {}".format(run)
+    if err is not None:
+        raise RuntimeError("Error executing {} in {}. {}".format(cmd, str(cwd), err))
+
+
+class RHVoiceBuild(build):
+    def run(self):
+        src_path = os.path.join(self.build_base, RHVOICE)
+        build_lib_lib = os.path.join(self.build_lib, PACKAGE_PATH, LIB)
+
+        self.mkpath(self.build_base)
+        self.mkpath(self.build_lib)
+        self.mkpath(build_lib_lib)
+
+        libraries_path = library_selector(src_path)
+        clone = [
+            [
+                "git",
+                "clone",
+                "--recurse-submodules",
+                "--depth=1",
+                "--branch",
+                RHVOICE_TAG,
+                RHVOICE_URL,
+                src_path,
+            ],
+            None,
+        ]
+        scons = [scons_selector(), src_path]
+
+        if not os.path.isdir(src_path):
+            # self.execute(executor, clone, "Clone {}".format(RHVOICE_URL))
+            pass
+        else:
+            self.warn("Use existing source data from {}".format(src_path))
+
+        if check_build(libraries_path) is None:
+            self.warn(
+                "Source already build? Use existing binary data from {}".format(
+                    src_path
+                )
+            )
+        else:
+            self.execute(executor, scons, "Compiling RHVoice...")
+
+        msg = check_build(libraries_path)
+        if msg is not None:
+            raise RuntimeError(msg)
+
+        if not self.dry_run:  # copy files
+            self.debug_print("Starting libraries copying..")
+            for target in libraries_path:
+                dst = os.path.join(build_lib_lib, os.path.basename(target))
+                self.debug_print("copying {} to {}...".format(target, dst))
+                dst = shutil.copy(target, dst)
+                self.debug_print("copy {} to {}".format(target, dst))
+        build.run(self)
+
+
+cmd_class["build"] = RHVoiceBuild
+
+
+def get_version() -> str:
+    version_file = "version"
+
+    def version_to_file(ver):
+        with open(version_file, mode="w") as fd:
+            fd.write(ver)
+
+    def version_from_file():
+        with open(version_file) as fd:
+            return fd.read().splitlines()[0]
+
+    def version_from_git():
+        cmd = ["git", "describe", "--abbrev=0", "--tags"]
+        try:
+            return subprocess.check_output(cmd).decode().splitlines()[0]
+        except Exception as e:
+            print("ERROR! Execute {}: {}".format(cmd, e))
+            return None
+
+    return "0.5.0"
+    # version = version_from_git()
+    # if not version:
+    #     version = version_from_file()
+    #     print("WARNING! Get version from a file: {}".format(version))
+    # else:
+    #     version_to_file(version)
+    # return version
+
+
+def get_long_description():
+    with open("README.md") as fh:
+        return fh.read()
+
+
+version_ = get_version()
+
+setup(
+    name="rhvoice-wrapper-bin",
+    version=version_,
+    packages=[PACKAGE_PATH],
+    package_data={PACKAGE_PATH: [os.path.join(LIB, "*")]},
+    url="https://github.com/Aculeasis/rhvoice-wrapper-bin",
+    license="GPLv3+",
+    author="Aculeasis",
+    author_email="amilpalimov2@ya.ru",
+    description="Provides RHVoice libraries for rhvoice-wrapper",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.4",
+    install_requires=["rhvoice-wrapper-data~={}".format(version_)],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: C++",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Multimedia :: Sound/Audio :: Speech",
+        "Topic :: Software Development :: Libraries",
+    ],
+    zip_safe=False,
+    cmdclass=cmd_class,
+    include_package_data=True,
+)
---
+++ data/setup.py
@@ -0,0 +1,224 @@
+import os
+import shutil
+import subprocess
+import sys
+from distutils.command.build import build
+
+from setuptools import setup
+
+ALL_VOICES = "--all-voices" in sys.argv and sys.argv.remove("--all-voices") is None
+PACKAGE_PATH = "rhvoice_wrapper_data"
+RHVOICE = "RHVoice"
+RHVOICE_TAG = "1.14.0"
+RHVOICE_URL = "https://github.com/RHVoice/RHVoice.git"
+DATA_DIR = "data"
+LICENSES_DIR = "licenses"
+BASE_VOICES = set(
+    "aleksandr anatol anna azamat bdl clb elena irina Leticia-F123 natalia natia nazgul slt spomenka "
+    "talgat".split(" ")
+)
+
+
+def check_build(data_paths):
+    for target in data_paths:
+        if not os.path.isdir(target):
+            return "Directory {} not found".format(target)
+
+
+def executor(cmd, cwd):
+    err = None
+    try:
+        run = subprocess.call(cmd, cwd=cwd)
+    except Exception as e:
+        err = e
+    else:
+        if run != 0:
+            err = "code: {}".format(run)
+    if err is not None:
+        raise RuntimeError("Error executing {} in {}. {}".format(cmd, str(cwd), err))
+
+
+class RHVoiceBuild(build):
+    def _copy_file(self, filename, src_path, dst_path):
+        def make_dst():
+            dst__ = os.path.join(dst_path, filename)
+            if os.path.exists(dst__):
+                for x in range(1, 9999):
+                    filename_2 = "{}-{}".format(filename, x)
+                    dst__ = os.path.join(dst_path, filename_2)
+                    if not os.path.exists(dst__):
+                        self.warn(
+                            "File {} already exists in {}, save is as {}!".format(
+                                filename, dst_path, filename_2
+                            )
+                        )
+                        break
+                else:
+                    raise RuntimeError("ok")
+            return dst__
+
+        src = os.path.join(src_path, filename)
+        if self.dry_run or not os.path.isfile(src):
+            return
+        if not os.path.exists(dst_path):
+            self.mkpath(dst_path)
+        dst = make_dst()
+        self.debug_print("copying {} to {}...".format(src, dst))
+        dst = shutil.copy(src, dst)
+        self.debug_print("copy {} to {}".format(src, dst))
+
+    def _remove_dst(self, dst: str):
+        if not self.dry_run and os.path.exists(dst):
+            self.debug_print("Existing path {}. deleting...".format(dst))
+            shutil.rmtree(dst, ignore_errors=True)
+
+    def _copy_voices(self, src: str, dst: str) -> list:
+        self.debug_print("Starting voices copying {} to {}...".format(src, dst))
+        voices = []
+        with os.scandir(src) as itr:
+            for x in itr:
+                if (ALL_VOICES or x.name in BASE_VOICES) and os.path.isdir(x.path):
+                    voices.append(x.name)
+        self._remove_dst(dst)
+        for voice in voices:
+            self._copy_dir(os.path.join(src, voice), os.path.join(dst, voice))
+        return voices
+
+    def _copy_licenses(self, src: str, dst: str, src_voices: str, voices: list):
+        self.debug_print("Starting licenses copying...")
+        self._copy_dir(src, dst)
+        for top_license in os.listdir(src):
+            if top_license.lower().startswith("license"):
+                self._copy_file(top_license, src, dst)
+        for voice in voices:
+            src_voice = os.path.join(src_voices, voice)
+            for target in os.listdir(src_voice):
+                if target.lower().startswith(("license", "readme")):
+                    self._copy_file(
+                        target, src_voice, os.path.join(dst, "voices", voice)
+                    )
+
+    def _copy_dir(self, src: str, dst: str) -> str:
+        def ignore(*_):
+            return ["16000", ".gitattributes", ".gitignore", "CMakeLists.txt", ".git"]
+
+        self.debug_print("Starting copying {} to {}...".format(src, dst))
+        self._remove_dst(dst)
+        if not self.dry_run:
+            dst = shutil.copytree(src, dst, ignore=ignore)
+            self.debug_print("copied {} to {}".format(src, dst))
+        return dst
+
+    def run(self):
+        src_path = os.path.join(self.build_base, RHVOICE)
+        dst_path = os.path.join(self.build_lib, PACKAGE_PATH)
+        targets = {
+            "voices": (
+                os.path.join(src_path, DATA_DIR, "voices"),
+                os.path.join(dst_path, DATA_DIR, "voices"),
+            ),
+            "languages": (
+                os.path.join(src_path, DATA_DIR, "languages"),
+                os.path.join(dst_path, DATA_DIR, "languages"),
+            ),
+            "licenses": (
+                os.path.join(src_path, LICENSES_DIR),
+                os.path.join(dst_path, LICENSES_DIR),
+            ),
+        }
+
+        self.mkpath(self.build_base)
+        self.mkpath(self.build_lib)
+        [self.mkpath(x) for (_, x) in targets.values()]
+
+        clone = [
+            [
+                "git",
+                "clone",
+                "--recurse-submodules",
+                "--depth=1",
+                "--branch",
+                RHVOICE_TAG,
+                RHVOICE_URL,
+                src_path,
+            ],
+            None,
+        ]
+
+        if not os.path.isdir(src_path):
+            # self.execute(executor, clone, 'Clone {}'.format(RHVOICE_URL))
+            pass
+        else:
+            self.warn("Use existing source data from {}".format(src_path))
+
+        msg = check_build([x for (x, _) in targets.values()])
+        if msg is not None:
+            raise RuntimeError(msg)
+
+        voices = self._copy_voices(*targets["voices"])
+        self._copy_licenses(*targets["licenses"], targets["voices"][0], voices=voices)
+        self._copy_dir(*targets["languages"])
+
+        build.run(self)
+
+
+def get_version() -> str:
+    version_file = "version"
+
+    def version_to_file(ver):
+        with open(version_file, mode="w") as fd:
+            fd.write(ver)
+
+    def version_from_file():
+        with open(version_file) as fd:
+            return fd.read().splitlines()[0]
+
+    def version_from_git():
+        cmd = ["git", "describe", "--abbrev=0", "--tags"]
+        try:
+            return subprocess.check_output(cmd).decode().splitlines()[0]
+        except Exception as e:
+            print("ERROR! Execute {}: {}".format(cmd, e))
+            return None
+
+    return "0.5.0"
+    # version = version_from_git()
+    # if not version:
+    #     version = version_from_file()
+    #     print("WARNING! Get version from a file: {}".format(version))
+    # else:
+    #     version_to_file(version)
+    # return version
+
+
+def get_long_description():
+    with open("README.md") as fh:
+        return fh.read()
+
+
+setup(
+    name="rhvoice-wrapper-data",
+    version=get_version(),
+    packages=[PACKAGE_PATH],
+    package_data={
+        PACKAGE_PATH: [os.path.join(x, "*") for x in (DATA_DIR, LICENSES_DIR)]
+    },
+    url="https://github.com/Aculeasis/rhvoice-wrapper-data",
+    license="GPLv3+",
+    author="Aculeasis",
+    author_email="amilpalimov2@ya.ru",
+    description="Provides RHVoice data for rhvoice-wrapper-bin",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.4",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Sound/Audio :: Speech",
+    ],
+    zip_safe=False,
+    cmdclass={"build": RHVoiceBuild},
+    include_package_data=True,
+)
